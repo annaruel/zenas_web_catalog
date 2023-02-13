1af1057d24cd9358eae950ce8c58bd5af00f8ead
annaruel <45943333+annaruel@users.noreply.github.com>
2023-02-12 19:21:34 -0500

    add query for db, role, schema

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas
 # Test connection 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT() ,CURRENT_REGION(), current_database(), current_role(), current_schema()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
